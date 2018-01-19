a28683fe161ca00903b4b12a35010370fb908f43
Oguzhan Uzman <ouzmann@gmail.com>
2018-01-19 19:58:37 +0300

    Initial commit

---
+++ src/main/java/com/oguzhanuzman/github/gardirop/GardiropApp.java
@@ -0,0 +1,11 @@
+package com.oguzhanuzman.github.gardirop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardiropApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GardiropApp.class, args);
+    }
+}
